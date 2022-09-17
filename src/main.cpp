c47a851cc0d9ef5517471de5d44468e01fd8abee
maki nohira <masayoshi55@live.jp>
2022-09-17 17:28:52 +0900

    esp32対応 MMA8451対応

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,41 @@
+
 #include <Arduino.h>
 
-#include "ArduinoSort.h"
-
 #include "JmaIntensity.hpp"
-#include "LED.hpp"
-#include "MCP3204.hpp"
+
 #include "Filter.hpp"
+#include <cmath>
+#ifdef ACE_SORTING
+#include <AceSorting.h>
+using ace_sorting::shellSortKnuth;
+#endif
+#ifndef ACE_SORTING
+#include <ArduinoSort.h>
+#endif
+
+#ifdef MMA8451
+#include <Adafruit_MMA8451.h>
+Adafruit_MMA8451 mma = Adafruit_MMA8451();
+#endif
+#ifdef MCP3204
+#include "MCP3204.hpp"
+MCP3204 ADC = MCP3204(SPISettings(115200, MSBFIRST, SPI_MODE0), D21);
+#endif
+#ifdef LED
+#include "LED.hpp"
+Led LED = Led();
+#endif
+
+#define SAMPLING_RATE 100
 
 // 震度を計算する頻度(1 ~ 100/秒)
 const int CALC_INTENSITY_RATE = 10;
 // 加速度を出力する頻度(0 ~ 100/秒)
 const int ACC_REPORT_RATE = 100;
 
-Led LED = Led();
-MCP3204 ADC = MCP3204(SPISettings(115200, MSBFIRST, SPI_MODE0), D21);
-auto ADJUST_PIN = D16;
+auto ADJUST_PIN = 16;
 
-const int samplingRate = 100;
+const int samplingRate = SAMPLING_RATE;
 const int bufferSize = samplingRate;
 
 Filter FILTER = Filter(samplingRate);
@@ -27,19 +46,36 @@ void setup()
 
     Serial.begin(115200);
 
+#ifdef MMA8451
+    if (!mma.begin())
+    {
+        Serial.println("Couldnt start");
+        while (1)
+            ;
+    }
+    mma.setRange(MMA8451_RANGE_2_G);
+    mma.writeRegister8(0x2A, 0x00 | 0x01 | 0x04); // LNOISE
+    mma.writeRegister8(0x2B, 0x10);
+#endif
+
+#ifndef MMA8451
     // TODO: これクラスの中に持っていったほうが良さそう
     SPI.setRX(D20);
     SPI.setCS(D21);
     SPI.setSCK(D18);
     SPI.setTX(D19);
     SPI.begin();
+#endif
 
+#ifdef LED
     LED.wakeup();
+#endif
 }
 
 // メモ: $はつけない
 void printNmea(const char *format, ...)
 {
+
     va_list arg;
     va_start(arg, format);
     char temp[64];
@@ -72,6 +108,7 @@ uint16_t offset[3];
 int offsetCounter = bufferSize * 1;
 bool isOffsetted = false;
 
+#ifdef MCP3204
 // オフセットを計算する
 void calcOffset(uint16_t *buf)
 {
@@ -79,6 +116,16 @@ void calcOffset(uint16_t *buf)
     offset[1] = buf[1];
     offset[2] = buf[2];
 }
+#endif
+
+#ifdef MMA8451
+void calcOffset(float *buf)
+{
+    offset[0] = buf[0];
+    offset[1] = buf[1];
+    offset[2] = buf[2];
+}
+#endif
 
 float computePGA(float *HPFilteredData)
 {
@@ -97,7 +144,14 @@ float computePGA(float *HPFilteredData)
 const int RETENTION_MICRO_SECONDS = 60 * 10 * 1000000;
 int x = 0, y = 0, z = 0;
 
+#ifdef MCP3204
 uint16_t rawData[3];
+#endif
+
+#ifdef MMA8451
+float rawData[3];
+#endif
+
 float compositeData[bufferSize];
 float compositeSortedData[bufferSize];
 float HPFilteredData[bufferSize * 3];
@@ -110,20 +164,35 @@ JmaIntensity maxIntensity;
 
 void loop()
 {
+
     auto startTime = micros();
 
     uint16_t index = frame % bufferSize;
 
-    // 計測した値を取得
+// 計測した値を取得
+#ifdef MMA8451
+    mma.read();
+    sensors_event_t event;
+    mma.getEvent(&event);
+#endif
+
     for (auto a = 0; a < 3; a++)
     {
-        uint16_t v = ADC.read(a);
 
+#ifdef MMA8451
+        rawData[a] = (a == 0) ? abs(event.acceleration.x) : (a == 1) ? abs(event.acceleration.y)
+                                                                : abs(event.acceleration.z);
+#endif
+#ifdef MCP3204
+        uint16_t v = ADC.read(a);
         // 極値を弾く
         if (v < 0 || v > 4096 || v == 1024 || v == 2048 || v == 3072)
             v = (uint16_t)offset[a];
-
         rawData[a] = v;
+
+#endif
+        //Serial.println(rawData[a]);
+        // Serial.println(v);
     }
 
     // オフセット計算
@@ -136,7 +205,13 @@ void loop()
 
     for (auto i = 0; i < 3; i++)
     {
+
+#ifdef MCP3204
         offsetSample[i] = (rawData[i] - offset[i]) / 1024.0f * 981;
+#endif
+#ifdef MMA8451
+        offsetSample[i] = (rawData[i] * 100.0);
+#endif
         newHPFilteredSample[i] = offsetSample[i];
         filteredDataSample[i] = offsetSample[i];
     }
@@ -161,7 +236,14 @@ void loop()
     if (isOffsetted && frame % (samplingRate / CALC_INTENSITY_RATE) == 0)
     {
         memcpy(compositeSortedData, compositeData, sizeof(compositeData[0]) * bufferSize);
+
+#ifdef ACE_SORTING
+        shellSortKnuth(compositeSortedData, bufferSize);
+#endif
+#ifndef ACE_SORTING
         sortArray(compositeSortedData, bufferSize);
+#endif
+
         auto gal = compositeSortedData[(int)(samplingRate * 0.7)];
         float pga = computePGA(HPFilteredData);
         printNmea("XSPGA,%.3f", pga);
@@ -177,22 +259,30 @@ void loop()
                 latestMaxTime = micros();
                 maxIntensity = latestIntensity;
             }
+#ifdef LED
             LED.blinkScale(latestIntensity, maxIntensity);
+#endif
         }
         else
         {
             printNmea("XSINT,%.3f,", gal);
+#ifdef LED
             LED.clear();
+#endif
         }
     }
 
     if (isOffsetted && frame % (samplingRate / 4) == 0 && latestIntensity < maxIntensity)
+#ifdef LED
         LED.toggle(maxIntensity);
+#endif
 
     if (!isOffsetted)
     {
         auto v = (JmaIntensity)(offsetCounter * 100 / (samplingRate * 5) % 10);
+#ifdef LED
         LED.blinkScale(v, v);
+#endif
     }
 
     if (frame >= samplingRate * 60)
@@ -203,13 +293,17 @@ void loop()
     auto workTime = micros() - startTime;
     auto sleepTime = 1000000 / samplingRate - workTime;
     if (sleepTime > 0)
-        sleep_us(sleepTime);
+#ifdef ESP32
+        delayMicroseconds(sleepTime);
+#endif
+#ifndef ESP32
+    sleep_us(sleepTime);
+#endif
 
     // if (frame % samplingRate == 0)
     // {
     //     printNmea("XTIME,%d,%d", workTime, sleepTime);
     // }
-
     if (digitalRead(ADJUST_PIN) && isOffsetted)
     {
         isOffsetted = false;
