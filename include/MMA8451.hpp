7eae3e17ad4ee46b1c9c51333aa5119a7d27265c
maki nohira <masayoshi55@live.jp>
2022-09-18 00:45:09 +0900

    save

--- include/MMA8451.hpp
+++ include/MMA8451.hpp
@@ -21,7 +21,7 @@ public:
             while (1)
                 ;
         }
-        mma.setRange(MMA8451_RANGE_2_G);
+        mma.setRange(MMA8451_RANGE_4_G);
         mma.writeRegister8(0x2A, 0x00 | 0x01 | 0x04); // LNOISE
         mma.writeRegister8(0x2B, 0x10);
     }
